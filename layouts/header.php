fe66cfaa653a88dc3b858724cca826aa723de12f
Levis Ble <levisble@gmail.com>
2025-04-13 09:22:48 +0000

    header nav actualisé

--- layouts/header.php
+++ layouts/header.php
@@ -18,12 +18,12 @@ $currentRoute = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
                 <ul class="cs_nav_list">
                   <li><a href="/" class="<?= $currentRoute === '' ? 'active' : '' ?>">Accueil</a></li>
                   <li class="menu-item-has-children <?= in_array($currentRoute, ['about', 'contact']) ? 'active' : '' ?>">
-                    <a href="#" aria-label="Menu link">Groupe Kacou</a>
-                    <ul>
-                      <li><a href="/about" class="<?= $currentRoute === 'about' ? 'active' : '' ?>" aria-label="Menu link">À propos</a></li>
-                      <li><a href="/contact" class="<?= $currentRoute === 'contact' ? 'active' : '' ?>" aria-label="Menu link">Contact</a></li>
-                    </ul>
-                  </li>
+                        <a href="#" class="<?= in_array($currentRoute, ['about', 'contact']) ? 'active' : '' ?>" aria-label="Menu link">Groupe Kacou</a>
+                        <ul>
+                            <li><a href="/about" class="<?= $currentRoute === 'about' ? 'active' : '' ?>" aria-label="Menu link">À propos</a></li>
+                             <li><a href="/contact" class="<?= $currentRoute === 'contact' ? 'active' : '' ?>" aria-label="Menu link">Contact</a></li>
+                        </ul>
+                </li>
 
                   <li><a href="/service" class="<?= $currentRoute === 'service' ? 'active' : '' ?>">Nos services</a></li>
                   <li><a href="/formation" class="<?= $currentRoute === 'formation' ? 'active' : '' ?>">Formations</a></li>
